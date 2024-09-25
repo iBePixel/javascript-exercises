14bfaf6b290d37b65be5cacf53844a93144bbb76
Elliott Lamb <elliottlamb123@gmail.com>
2024-09-25 23:39:56 +0100

    solution 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toRemove) {
+
+    for (let i = 0; i < array.length; i++) {
+        for (let g = 0; g < toRemove.length; g++) {
+            if (array[i] === toRemove[g]) {
+                array.splice(i, 1)
+                i = 0;
+            }
+        }
+    }
+    console.log(toRemove.length)
+    return array;
 };
 
 // Do not edit below this line
