6c2e4a1798b8cfbf8df15257029561c13bfda2c5
Elliott Lamb <elliottlamb123@gmail.com>
2024-09-26 00:01:00 +0100

    05 first 2 solutions working

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let value = start; 
+    for (let i = start + 1; i <= end; i++){
+        value = value + i;
+    }
+    console.log(value)
+    return value
 };
 
 // Do not edit below this line
