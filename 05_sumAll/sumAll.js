a75e01b8456b1b529a3b323d5eb5c4b4c1724694
Elliott Lamb <elliottlamb123@gmail.com>
2024-09-26 22:26:27 +0100

    Solution 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,29 @@
-const sumAll = function(start, end) {
-    let value = start; 
-    for (let i = start + 1; i <= end; i++){
-        value = value + i;
+const sumAll = function (start, end) {
+
+    let value;
+    let finish;
+
+    if (start > 0 && end > 0 && typeof start === 'number' && typeof end === 'number' && (start % 1) === 0 && (end % 1) === 0) {
+        if (start < end) {
+            value = start;
+            finish = end;
+        }
+        else if (end < start) {
+            value = end;
+            finish = start;
+        }
+
+        for (let i = value + value; i <= finish; i++) {
+            value = value + i;
+        }
+        return value
     }
-    console.log(value)
-    return value
-};
+
+    else {
+        return 'ERROR';
+    }
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
