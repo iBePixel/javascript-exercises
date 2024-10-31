0ffe8a2cc876afe6e2d70b6f9adec5260cd90138
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2024-10-31 21:10:27 +0000

    Updated functions for 08, all tests passing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    result = num1 - num2;
+    return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  result = arr.reduce((sum, current) => sum + current, 0);
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = arr.reduce((sum, current) => sum * current);
+	return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let result = base;
+  
+  for (let i = 1; i < power; i++){
+    result = result * base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++){
+    result = result * i;
+  }
+  return result;
 	
 };
 
