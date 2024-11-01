505759be55275ad7df492ed30406aa5aeccdd846
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2024-11-01 00:32:36 +0000

    09 all tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversed = string.toLowerCase().split('').filter(char => {return /[a-zA-Z0-9]/.test(char)}).reverse().join("");
+    const alphanumericalString = string.toLowerCase().split('').filter(char => {return /[a-zA-Z0-9]/.test(char)}).join('');
+    
+    return reversed == alphanumericalString;
+    
 };
 
 // Do not edit below this line
