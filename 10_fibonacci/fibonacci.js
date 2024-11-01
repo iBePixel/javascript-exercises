56df05c530eaad76af8878fb21b2799388a6842d
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-11-01 12:32:33 +0000

    exercise 10 - all tests passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let max = num
+    let num1 = 1
+    let num2 = 0
+    let result = 1;
 
+    if (num < 0){
+        return "OOPS"
+    }
+    else if (num.typeOf == 'string'){
+        max = parseInt(max);
+    }
+    else if (num == 0){
+        return 0
+    }
+
+    for(let i = 1; i < max; i++){
+        result = num1 + num2;
+        num2 = num1;
+        num1 = result;
+    }
+
+
+    return result;
 };
 
 // Do not edit below this line
