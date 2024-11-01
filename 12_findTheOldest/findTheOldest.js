0411f651ac0cea88e2e6ec743efee3e2b659226d
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-11-01 14:35:40 +0000

    JS exercise 12 - all tests passing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    workingArr = arr
+    let result = arr[0]
+    let ages = []
+    let year = new Date().getFullYear();
 
+    workingArr.forEach(element => {
+        if (element.yearOfDeath == undefined){
+            element.yearOfDeath = year;
+        }
+    });
+
+    workingArr.forEach(element => {
+        age = element.yearOfDeath - element.yearOfBirth;
+        ages.push(age)
+        console.log(age)
+    });
+
+    let topAge = ages[0];
+
+    for (let i = 1; i < ages.length; i++){
+        if(ages[i] > topAge){
+            result = arr[i]
+            topAge = ages[i]
+        }
+    }
+
+    console.log(topAge);
+    return result;
 };
 
 // Do not edit below this line
