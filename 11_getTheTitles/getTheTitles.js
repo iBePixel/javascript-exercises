7d4f7bb0c1f6dec90e39e41bb653fa87890cd1f6
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-11-01 13:08:52 +0000

    JS 11 - tests passing

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let result = []
+    arr.map(item => result.push(item.title));
 
+    return result;
 };
 
 // Do not edit below this line
